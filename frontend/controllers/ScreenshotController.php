b76ec75c232f33c9fe2b60b143d274f1f5704a90
Roman Pustovalov <romkafrfr@gmail.com>
2019-11-28 13:25:01 +0300

    создан контроллер для скриншотов

--- frontend/controllers/ScreenshotController.php
+++ frontend/controllers/ScreenshotController.php
@@ -3,12 +3,33 @@
 
 namespace frontend\controllers;
 
+use frontend\classes\GoogleScreenshotService;
+use frontend\classes\ImageSaver;
+use frontend\classes\SShotScreenshotService;
 use yii\rest\Controller;
 
 class ScreenshotController extends Controller
 {
-    public function actionIndex()
+    //Google PageSpeed Insights
+    public function actionIndex($url)
     {
-        return $this->asJson('ok');
+        $screenshot = new GoogleScreenshotService($url);
+        $result = $screenshot->createRequest();
+
+        return $this->asJson([
+            'url' => $url,
+            'imageLink' => (new ImageSaver())->save($screenshot->getScreenshot()),
+        ]);
+    }
+
+    public function actionSShot($url, $resolution = '1024', $width = '800')
+    {
+        $screenshot = new SShotScreenshotService($url, $resolution, $width);
+        $result = $screenshot->createRequest();
+
+        return $this->asJson([
+            'url' => $url,
+            'imageLink' => (new ImageSaver())->save($screenshot->getScreenshot()),
+        ]);
     }
 }
